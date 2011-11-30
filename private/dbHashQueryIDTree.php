83f1db765713899aac27166e6a1ecf24c392b275
Andrew Rivett <veggiefrog@gmail.com>
2011-11-30 15:38:26 -0500

    Added db output to hash tree by ID

---
+++ private/dbHashQueryIDTree.php
@@ -0,0 +1,106 @@
+<?php
+//
+// Description
+// -----------
+// This function will query the database, and build a hash tree based
+// on the elements of the $tree variable.  This is similar to 
+// dbHashQueryTree but does not build the sub container xml friendly model,
+// this function returns indexed hash tree structure, where nodes are the ID's from the database.
+// 
+// FIXME: add documentation
+//
+// Info
+// ----
+// status:			beta
+//
+// Arguments
+// ---------
+// ciniki:			The ciniki data structure.
+// strsql: 			The SQL string to query the database.
+// module:			The module name the query is acting on.
+// container_name:	The name of the xml/hash tag to return the data under, 
+//					when there is only one row returned.
+// col_name:		The column to be used as the row ID within the result.
+//
+function ciniki_core_dbHashQueryIDTree($ciniki, $strsql, $module, $tree) {
+	//
+	// Open a connection to the database if one doesn't exist.  The
+	// dbConnect function will return an open connection if one 
+	// exists, otherwise open a new one
+	//
+	$rc = ciniki_core_dbConnect($ciniki, $module);
+	if( $rc['stat'] != 'ok' ) {
+		return $rc;
+	}
+
+	$dh = $rc['dh'];
+
+	//
+	// Prepare and Execute Query
+	//
+	$result = mysql_query($strsql, $dh);
+	if( $result == false ) {
+		return array('stat'=>'fail', 'err'=>array('pkg'=>'ciniki', 'code'=>'183', 'msg'=>'Database Error', 'pmsg'=>mysql_error($dh)));
+	}
+
+	//
+	// Check if any rows returned from the query
+	//
+	$rsp = array('stat'=>'ok');
+	$rsp['num_rows'] = 0;
+	$rsp['num_cols'] = 0;
+
+	//
+	// Build array of rows
+	//
+	$prev = array();
+	for($i=0;$i<count($tree);$i++) {
+		$prev[$i] = null;
+	}
+	while( $row = mysql_fetch_assoc($result) ) {
+		// 
+		// Check if we have anything new at each depth
+		//
+		$data = &$rsp;
+		for($i=0;$i<count($tree);$i++) {
+			if( $i > 0 ) {
+				// $data = $data[$tree[$i]['container'];
+			}
+			// error_log($tree[$i]['fname'] . ' = ' . $row[$tree[$i]['fname']]);
+			if( is_null($row[$tree[$i]['fname']]) ) {
+				continue;
+			}
+			if( $prev[$i] != $row[$tree[$i]['fname']] ) {
+				// Reset all this depth and below
+				for($j=$i+1;$j<count($tree);$j++) {
+					$prev[$j] = null;
+				}
+				//
+				// Check if this is a simple value, or if there are fields
+				//
+				if( isset($tree[$i]['value']) && $tree[$i]['value'] != '' ) {
+					$data[$tree[$i]['container']][$row[$tree[$i]['fname']]]	= $row[$tree[$i]['value']];
+				} else {
+					// Check if container exists
+					if( !isset($data[$tree[$i]['container']]) ) {
+						$data[$tree[$i]['container']] = array();
+					}
+					$data[$tree[$i]['container']][$row[$tree[$i]['fname']]] = array();
+						
+					// Copy Data
+					foreach($tree[$i]['fields'] as $field) {
+						$data[$tree[$i]['container']][$row[$tree[$i]['fname']]][$field] = $row[$field];
+					}
+					$data = &$data[$tree[$i]['container']][$row[$tree[$i]['fname']]];
+				}
+			}
+			else {
+				$data = &$data[$tree[$i]['container']][$row[$tree[$i]['fname']]];
+			}
+			$prev[$i] = $row[$tree[$i]['fname']];
+		}
+	}
+
+	return $rsp;
+}
+?>

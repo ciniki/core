339ca1672f659d0354d943f2e939c05196588969
Andrew Rivett <veggiefrog@gmail.com>
2013-01-05 20:07:13 -0500

    Fixed SQL

--- private/syncUpdateUUIDMap.php
+++ private/syncUpdateUUIDMap.php
@@ -13,9 +13,9 @@ function ciniki_core_syncLoad($ciniki, $business_id, $sync, $module, $remote_uui
 
 	$strsql = "INSERT INTO ciniki_business_sync_uuidmaps (sync_id, module, "
 		. "remote_uuid, local_uuid) VALUES ("
-		. "'" . ciniki_core_dbQuote($ciniki, $sync['id']) . "' "
-		. "'" . ciniki_core_dbQuote($ciniki, $module) . "' "
-		. "'" . ciniki_core_dbQuote($ciniki, $remote_uuid) . "' "
+		. "'" . ciniki_core_dbQuote($ciniki, $sync['id']) . "', "
+		. "'" . ciniki_core_dbQuote($ciniki, $module) . "', "
+		. "'" . ciniki_core_dbQuote($ciniki, $remote_uuid) . "', "
 		. "'" . ciniki_core_dbQuote($ciniki, $local_uuid) . "' "
 		. ")";
 	$rc = ciniki_core_dbInsert($ciniki, $strsql, 'ciniki.businesses');

eb48cbc429a18460ebded4a41ac33e7dd8924226
Andrew Rivett <veggiefrog@gmail.com>
2013-01-05 20:43:15 -0500

    Fixed UpdateUUID change module to table name

--- private/syncUpdateUUIDMap.php
+++ private/syncUpdateUUIDMap.php
@@ -9,14 +9,14 @@
 // Arguments
 // ---------
 //
-function ciniki_core_syncUpdateUUIDMap(&$ciniki, &$sync, $business_id, $table, $remote_uuid, $local_uuid) {
+function ciniki_core_syncUpdateUUIDMap(&$ciniki, &$sync, $business_id, $table, $remote_uuid, $local_id) {
 
 	$strsql = "INSERT INTO ciniki_business_sync_uuidmaps (sync_id, table, "
-		. "remote_uuid, local_uuid) VALUES ("
+		. "remote_uuid, local_id) VALUES ("
 		. "'" . ciniki_core_dbQuote($ciniki, $sync['id']) . "', "
 		. "'" . ciniki_core_dbQuote($ciniki, $table) . "', "
 		. "'" . ciniki_core_dbQuote($ciniki, $remote_uuid) . "', "
-		. "'" . ciniki_core_dbQuote($ciniki, $local_uuid) . "' "
+		. "'" . ciniki_core_dbQuote($ciniki, $local_id) . "' "
 		. ")";
 	ciniki_core_loadMethod($ciniki, 'ciniki', 'core', 'private', 'dbInsert');
 	$rc = ciniki_core_dbInsert($ciniki, $strsql, 'ciniki.businesses');
@@ -27,11 +27,11 @@ function ciniki_core_syncUpdateUUIDMap(&$ciniki, &$sync, $business_id, $table, $
 		return $rc;
 	}
 
-	if( !isset($sync['uuidmaps']['ciniki.users']) ) {
-		$sync['uuidmaps']['ciniki.users'] = array();
+	if( !isset($sync['uuidmaps'][$table]) ) {
+		$sync['uuidmaps'][$table] = array();
 	}
 
-	$sync['uuidmaps']['ciniki.users'][$remote_uuid] = $local_uuid;
+	$sync['uuidmaps'][$table][$remote_uuid] = $local_id;
 
 	return array('stat'=>'ok');
 }

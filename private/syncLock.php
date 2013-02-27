05cbebfff39cfbefd7e81fe8f37a38fb1fc76c83
Andrew Rivett <veggiefrog@gmail.com>
2013-02-26 20:03:17 -0500

    Removed duplicate error numbers

--- private/syncLock.php
+++ private/syncLock.php
@@ -12,7 +12,7 @@
 function ciniki_core_syncLock($ciniki, $business_id, $sync_id) {
 
 	if( !isset($ciniki['config']['ciniki.core']['sync.lock_dir']) ) {
-		return array('stat'=>'fail', 'err'=>array('pkg'=>'ciniki', 'code'=>'999', 'msg'=>'No sync lock dir specified'));
+		return array('stat'=>'fail', 'err'=>array('pkg'=>'ciniki', 'code'=>'281', 'msg'=>'No sync lock dir specified'));
 	}
 
 	$lockfile = $ciniki['config']['ciniki.core']['sync.lock_dir'] . '/sync-' . $sync_id . '.lck';
@@ -22,7 +22,7 @@ function ciniki_core_syncLock($ciniki, $business_id, $sync_id) {
 	}
 
 	if( touch($lockfile) == false ) {
-		return array('stat'=>'fail', 'err'=>array('pkg'=>'ciniki', 'code'=>'999', 'msg'=>'Unable to set sync lock'));
+		return array('stat'=>'fail', 'err'=>array('pkg'=>'ciniki', 'code'=>'282', 'msg'=>'Unable to set sync lock'));
 	}
 
 	return array('stat'=>'ok');

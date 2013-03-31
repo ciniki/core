9c52258096805eff4ce2a6f2a2c367df7e8000c2
Andrew Rivett <veggiefrog@gmail.com>
2013-03-31 10:27:19 -0400

    Changed logMsg check for command line

--- private/logMsg.php
+++ private/logMsg.php
@@ -11,7 +11,8 @@
 //
 function ciniki_core_logMsg($ciniki, $lvl, $msg) {
 
-	if( isset($_SERVER['argc']) ) {
+//	if( isset($_SERVER['argc']) ) {
+	if( php_sapi_name() == 'cli' ) {
 		error_log('[' . date('d/M/Y:H:i:s O') . '] ' . $msg);
 	} else {
 		error_log($msg);

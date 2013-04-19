a1038c6584a2b43822fa360bee1fe1aabcc363ca
Andrew Rivett <veggiefrog@gmail.com>
2013-04-18 23:13:35 -0400

    Added stub function to get history for a list of items

---
+++ private/dbGetModuleHistoryList.php
@@ -0,0 +1,105 @@
+<?php
+//
+// Description
+// -----------
+//
+// Arguments
+// ---------
+// ciniki:
+// module:			The name of the module for the transaction, which should include the 
+//					package in dot notation.  Example: ciniki.artcatalog
+//
+//
+function ciniki_core_dbGetModuleHistoryList($ciniki, $module, $history_table, $business_id, $table_name, $table_key, $table_field, $table_id_field) {
+	//
+	// Open a connection to the database if one doesn't exist.  The
+	// dbConnect function will return an open connection if one 
+	// exists, otherwise open a new one
+	//
+
+	// *********************************
+	// FIXME:  THIS FUNCTION IS NOT FINISHED!!!!!!!!!!!!
+	// *********************************
+	return array('stat'=>'ok', 'history'=>array());
+
+
+
+
+
+
+	//
+	// Get the history log from ciniki_core_change_logs table.
+	//
+	ciniki_core_loadMethod($ciniki, 'ciniki', 'users', 'private', 'datetimeFormat');
+	ciniki_core_loadMethod($ciniki, 'ciniki', 'core', 'private', 'dbQuote');
+	ciniki_core_loadMethod($ciniki, 'ciniki', 'core', 'private', 'dbQuoteList');
+	ciniki_core_loadMethod($ciniki, 'ciniki', 'core', 'private', 'dbQueryList');
+	ciniki_core_loadMethod($ciniki, 'ciniki', 'core', 'private', 'dbParseAge');
+
+	//
+	// Get the list of table_keys we're interested in
+	// This query is broken into three stages for speed
+	//
+	$strsql = "SELECT DISTINCT table_key "
+		. "FROM $history_table "
+		. "WHERE business_id = '" . ciniki_core_dbQuote($ciniki, $business_id) . "' "
+		. "AND table_field = '" . ciniki_core_dbQuote($ciniki, $table_id_field) . "' "
+		. "AND new_value = '" . ciniki_core_dbQuote($ciniki, $table_key) . "' "
+		. "";
+	$rc = ciniki_core_dbQueryList($ciniki, $strsql, $module, 'keys', 'table_key');	
+	if( $rc['stat'] != 'ok' ) {
+		return $rc;
+	}
+	if( !isset($rc['keys']) || count($rc['keys']) == 0 ) {
+		return array('stat'=>'ok', 'history'=>array());
+	}
+	$keys = $rc['keys'];
+
+	//
+	// Finally get the history
+	//
+	$date_format = ciniki_users_datetimeFormat($ciniki);
+	$strsql = "SELECT user_id, DATE_FORMAT(log_date, '" . ciniki_core_dbQuote($ciniki, $date_format) . "') as date, "
+		. "CAST(UNIX_TIMESTAMP(UTC_TIMESTAMP())-UNIX_TIMESTAMP(log_date) as DECIMAL(12,0)) as age, "
+		. "action, "
+		. "table_key, "
+		. "table_field, "
+		. "ciniki_users.display_name AS user_display_name, "
+		. "new_value as value "
+		. "FROM " . ciniki_core_dbQuote($ciniki, $history_table) . " "
+		. "LEFT JOIN ciniki_users ON ($history_table.user_id = ciniki_users.id) "
+		. "WHERE business_id ='" . ciniki_core_dbQuote($ciniki, $business_id) . "' "
+		. "AND table_name = '" . ciniki_core_dbQuote($ciniki, $table_name) . "' "
+		. "AND (table_field = '" . ciniki_core_dbQuote($ciniki, $table_field) . "' OR table_field = '*') "
+		. "AND table_key IN (" . ciniki_core_dbQuoteList($ciniki, $keys) . ") "
+		. "ORDER BY $history_table.log_date ASC "
+		. "";
+	ciniki_core_loadMethod($ciniki, 'ciniki', 'core', 'private', 'dbHashQuery');
+	$rc = ciniki_core_dbHashQuery($ciniki, $strsql, $module, 'row');
+	if( $rc['stat'] != 'ok' ) {
+		return $rc;
+	}
+
+	//
+	// Build the history based on additions (action:1) and deletions(action:3)
+	//
+	$subscriptions = array();
+	$history = array();
+	foreach($rc['rows'] as $row) {
+		if( $row['table_field'] == $table_field ) {
+			$subscriptions[$row['table_key']] = $row['value'];
+			$history[] = array('action'=>array('user_id'=>$row['user_id'], 'date'=>$row['date'], 
+				'action'=>'1', 'value'=>$row['value'], 
+				'age'=>ciniki_core_dbParseAge($ciniki, $row['age']), 
+				'user_display_name'=>$row['user_display_name']));
+		} elseif( $row['table_field'] == '*' && isset($subscriptions[$row['table_key']]) ) {
+			$history[] = array('action'=>array('user_id'=>$row['user_id'], 'date'=>$row['date'], 
+				'action'=>'3', 'value'=>$subscriptions[$row['table_key']], 
+				'age'=>ciniki_core_dbParseAge($ciniki, $row['age']), 
+				'user_display_name'=>$row['user_display_name']));
+		}
+	}
+
+	return array('stat'=>'ok', 'history'=>array_reverse($history));
+}
+?>
--- private/emailQueueProcess.php
+++ private/emailQueueProcess.php
@@ -29,22 +29,59 @@ function ciniki_core_emailQueueProcess(&$ciniki) {
 			require_once($ciniki['config']['ciniki.core']['lib_dir'] . '/PHPMailer/class.smtp.php');
 
 			$mail = new PHPMailer;
-
 			$mail->IsSMTP();
-			$mail->Host = $ciniki['config']['ciniki.core']['system.smtp.servers'];
-			$mail->SMTPAuth = true;
-			$mail->Username = $ciniki['config']['ciniki.core']['system.smtp.username'];
-			$mail->Password = $ciniki['config']['ciniki.core']['system.smtp.password'];
-			$mail->SMTPSecure = $ciniki['config']['ciniki.core']['system.smtp.secure'];
-			$mail->Port = $ciniki['config']['ciniki.core']['system.smtp.port'];
 
-			$mail->From = $ciniki['config']['ciniki.core']['system.email'];
-			$mail->FromName = $ciniki['config']['ciniki.core']['system.email.name'];
+			$use_config = 'yes';
+			if( isset($email['business_id']) ) {
+				ciniki_core_loadMethod($ciniki, 'ciniki', 'mail', 'private', 'getSettings');
+				$rc = ciniki_mail_getSettings($ciniki, $email['business_id']);
+				if( $rc['stat'] == 'ok' && isset($rc['settings'])
+					&& isset($rc['settings']['smtp-servers']) && $rc['settings']['smtp-servers'] != ''
+					&& isset($rc['settings']['smtp-username']) && $rc['settings']['smtp-username'] != ''
+					&& isset($rc['settings']['smtp-password']) && $rc['settings']['smtp-password'] != ''
+					&& isset($rc['settings']['smtp-secure']) && $rc['settings']['smtp-secure'] != ''
+					&& isset($rc['settings']['smtp-port']) && $rc['settings']['smtp-port'] != ''
+					) {
+					$mail->Host = $rc['settings']['smtp-servers'];
+					$mail->SMTPAuth = true;
+					$mail->Username = $rc['settings']['smtp-username'];
+					$mail->Password = $rc['settings']['smtp-password'];
+					$mail->SMTPSecure = $rc['settings']['smtp-secure'];
+					$mail->Port = $rc['settings']['smtp-port'];
+					$use_config = 'no';
+
+					if( isset($rc['settings']['smtp-secure']) && $rc['settings']['smtp-secure'] != ''
+						&& isset($rc['settings']['smtp-port']) && $rc['settings']['smtp-port'] != '' ) {
+						$mail->From = $rc['settings']['smtp-from-address'];
+						$mail->FromName = $rc['settings']['smtp-from-name'];
+					} else {
+						$mail->From = $ciniki['config']['ciniki.core']['system.email'];
+						$mail->FromName = $ciniki['config']['ciniki.core']['system.email.name'];
+					}
+				}
+			} else {
+				$mail->Host = $ciniki['config']['ciniki.core']['system.smtp.servers'];
+				$mail->SMTPAuth = true;
+				$mail->Username = $ciniki['config']['ciniki.core']['system.smtp.username'];
+				$mail->Password = $ciniki['config']['ciniki.core']['system.smtp.password'];
+				$mail->SMTPSecure = $ciniki['config']['ciniki.core']['system.smtp.secure'];
+				$mail->Port = $ciniki['config']['ciniki.core']['system.smtp.port'];
+
+				$mail->From = $ciniki['config']['ciniki.core']['system.email'];
+				$mail->FromName = $ciniki['config']['ciniki.core']['system.email.name'];
+			}
+
 			$mail->AddAddress($email['to']);
 
-			$mail->IsHTML(false);
 			$mail->Subject = $email['subject'];
-			$mail->Body = $email['textmsg'];
+			if( isset($email['htmlmsg']) && $email['htmlmsg'] != '' ) {
+				$mail->IsHTML(true);
+				$mail->Body = $email['htmlmsg'];
+				$mail->AltBody = $email['textmsg'];
+			} else {
+				$mail->IsHTML(false);
+				$mail->Body = $email['textmsg'];
+			}
 
 			if( !$mail->Send() ) {
 				error_log("MAIL-ERR: [" . $email['to'] . "] " . $mail->ErrorInfo);

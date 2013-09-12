9c8e10c023e79d1f65b98dab385627f6befba7db
Andrew Rivett <veggiefrog@gmail.com>
2013-09-12 17:56:39 -0400

    Modified thread functions

--- private/threadGetFollowups.php
+++ private/threadGetFollowups.php
@@ -33,19 +33,34 @@ function ciniki_core_threadGetFollowups($ciniki, $module, $table, $prefix, $id, 
 	ciniki_core_loadMethod($ciniki, 'ciniki', 'users', 'private', 'timezoneOffset');
 	$utc_offset = ciniki_users_timezoneOffset($ciniki);
 
+	// Validate table and prefix
+	if( preg_match('/[^_a-z]/', $table) ) {
+//		|| preg_match('/[^_a_z]/', $prefix) ) {
+		return array('stat'=>'fail', 'err'=>array('pkg'=>'ciniki', 'code'=>'1336', 'msg'=>'Invalid database parameters' . $table . $prefix));
+	}
+
 	// 
 	// Setup the SQL statement to insert the new thread
 	//
 	$datetime_format = ciniki_users_datetimeFormat($ciniki);
-	$strsql = "SELECT id, " . ciniki_core_dbQuote($ciniki, "{$prefix}_id") . ", "
-		. "user_id, "
-		. "DATE_FORMAT(CONVERT_TZ(date_added, '+00:00', '" . ciniki_core_dbQuote($ciniki, $utc_offset) . "'), '" . ciniki_core_dbQuote($ciniki, $datetime_format) . "') AS date_added, "
-		. "CAST(UNIX_TIMESTAMP(UTC_TIMESTAMP())-UNIX_TIMESTAMP(date_added) as DECIMAL(12,0)) as age, content "
-		. "FROM " . ciniki_core_dbQuote($ciniki, $table) . " "
+	$strsql = "SELECT {$table}.id, " . ciniki_core_dbQuote($ciniki, "{$prefix}_id") . ", "
+		. "{$table}.user_id, "
+		. "DATE_FORMAT(CONVERT_TZ({$table}.date_added, '+00:00', '" . ciniki_core_dbQuote($ciniki, $utc_offset) . "'), '" . ciniki_core_dbQuote($ciniki, $datetime_format) . "') AS date_added, "
+		. "CAST(UNIX_TIMESTAMP(UTC_TIMESTAMP())-UNIX_TIMESTAMP({$table}.date_added) as DECIMAL(12,0)) as age, "
+		. "{$table}.content, "
+		. "ciniki_users.display_name AS user_display_name "
+		. "FROM {$table} "
+		. "LEFT JOIN ciniki_users ON (" . ciniki_core_dbQuote($ciniki, $table) . ".user_id = ciniki_users.id ) "
 		. "WHERE " . ciniki_core_dbQuote($ciniki, "{$prefix}_id") . " = '" . ciniki_core_dbQuote($ciniki, $id) . "' "
 		. "ORDER BY " . ciniki_core_dbQuote($ciniki, $table) . ".date_added ASC "
 		. "";
-	
-	return ciniki_core_dbRspQueryPlusDisplayNames($ciniki, $strsql, $module, 'followups', 'followup', array('stat'=>'ok', 'followups'=>array()));
+
+	ciniki_core_loadMethod($ciniki, 'ciniki', 'core', 'private', 'dbHashQueryTree');	
+	$rc = ciniki_core_dbHashQueryTree($ciniki, $strsql, $module, array(
+		array('container'=>'followups', 'fname'=>'id', 'name'=>'followup',
+			'fields'=>array('id', 'user_id', 'date_added', 'age', 'content', 'user_display_name')),
+		));
+	return $rc;	
+//	return ciniki_core_dbRspQueryPlusDisplayNames($ciniki, $strsql, $module, 'followups', 'followup', array('stat'=>'ok', 'followups'=>array()));
 }
 ?>

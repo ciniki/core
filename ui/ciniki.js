42f7487b88eae02d66d1d4a23234683128daf4d0
Andrew Rivett <veggiefrog@gmail.com>
2015-08-10 21:45:00 -0400

    Added check in rgbtoHex for blank string

--- ui/ciniki.js
+++ ui/ciniki.js
@@ -779,6 +779,7 @@ M.dateFormatWD = function(d) {
 M.rgbToHex = function(rgb) {
 	var hexDigits = ["0","1","2","3","4","5","6","7","8","9","a","b","c","d","e","f"];
 	rgb = rgb.match(/^rgb\((\d+),\s*(\d+),\s*(\d+)\)$/);
+	if( rgb == null ) { return ''; }
 	function hex(x) {
 		return isNaN(x) ? "00" : hexDigits[(x - x % 16) / 16] + hexDigits[x % 16];
 	}

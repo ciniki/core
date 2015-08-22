d96c7392ae2d9bef6c8b5ea6fe41c7cdb7ca4de5
Andrew Rivett <veggiefrog@gmail.com>
2015-08-21 21:10:27 -0400

    Updated makePermalink to handle french and other special characters converting them to plain english

--- private/makePermalink.php
+++ private/makePermalink.php
@@ -13,7 +13,18 @@
 // -------
 //
 function ciniki_core_makePermalink($ciniki, $str) {
-	$newstr = preg_replace('/[ \/]/', '-', preg_replace('/[^a-z0-9 \-\/]/', '', strip_tags(strtolower($str))));
+	$normalizeChars = array(
+	    'Š'=>'S', 'š'=>'s', 'Ð'=>'Dj','Ž'=>'Z', 'ž'=>'z', 'À'=>'A', 'Á'=>'A', 'Â'=>'A', 'Ã'=>'A', 'Ä'=>'A',
+		'Å'=>'A', 'Æ'=>'A', 'Ç'=>'C', 'È'=>'E', 'É'=>'E', 'Ê'=>'E', 'Ë'=>'E', 'Ì'=>'I', 'Í'=>'I', 'Î'=>'I',
+		'Ï'=>'I', 'Ñ'=>'N', 'Ò'=>'O', 'Ó'=>'O', 'Ô'=>'O', 'Õ'=>'O', 'Ö'=>'O', 'Ø'=>'O', 'Ù'=>'U', 'Ú'=>'U',
+		'Û'=>'U', 'Ü'=>'U', 'Ý'=>'Y', 'Þ'=>'B', 'ß'=>'Ss','à'=>'a', 'á'=>'a', 'â'=>'a', 'ã'=>'a', 'ä'=>'a',
+		'å'=>'a', 'æ'=>'a', 'ç'=>'c', 'è'=>'e', 'é'=>'e', 'ê'=>'e', 'ë'=>'e', 'ì'=>'i', 'í'=>'i', 'î'=>'i',
+		'ï'=>'i', 'ð'=>'o', 'ñ'=>'n', 'ò'=>'o', 'ó'=>'o', 'ô'=>'o', 'õ'=>'o', 'ö'=>'o', 'ø'=>'o', 'ù'=>'u',
+		'ú'=>'u', 'û'=>'u', 'ý'=>'y', 'ý'=>'y', 'þ'=>'b', 'ÿ'=>'y', 'ƒ'=>'f', 'ü'=>'u', 
+		'ă'=>'a', 'î'=>'i', 'â'=>'a', 'ș'=>'s', 'ț'=>'t', 'Ă'=>'A', 'Î'=>'I', 'Â'=>'A', 'Ș'=>'S', 'Ț'=>'T',
+		);
+	$newstr = strtr($str, $normalizeChars);
+	$newstr = preg_replace('/[ \/]/', '-', preg_replace('/[^a-z0-9 \-\/]/', '', strip_tags(strtolower($newstr))));
 	// Remove multiple replaced characters to a single dash, looks better in url
 	$newstr = preg_replace('/\-\-+/', '-', $newstr);
 	return $newstr;

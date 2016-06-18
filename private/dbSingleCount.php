1c75b91be3fbeef83a0347cdaf8930409d2aec74
Andrew Rivett <veggiefrog@gmail.com>
2016-06-17 20:27:51 -0400

    Converted tabs to spaces

--- private/dbSingleCount.php
+++ private/dbSingleCount.php
@@ -7,49 +7,49 @@
 //
 // Arguments
 // ---------
-// ciniki: 				The ciniki data structure with current session.
-// strsql:				The SQL string to query the database with.
-// module:				The name of the module for the transaction, which should include the 
-//						package in dot notation.  Example: ciniki.artcatalog
-// container_name:		The container name to attach the data when only one row returned.
+// ciniki:                 The ciniki data structure with current session.
+// strsql:                The SQL string to query the database with.
+// module:                The name of the module for the transaction, which should include the 
+//                        package in dot notation.  Example: ciniki.artcatalog
+// container_name:        The container name to attach the data when only one row returned.
 //
 function ciniki_core_dbSingleCount(&$ciniki, $strsql, $module, $container_name) {
-	//
-	// Check connection to database, and open if necessary
-	//
-	$rc = ciniki_core_dbConnect($ciniki, $module);
-	if( $rc['stat'] != 'ok' ) {
-		return $rc;
-	}
+    //
+    // Check connection to database, and open if necessary
+    //
+    $rc = ciniki_core_dbConnect($ciniki, $module);
+    if( $rc['stat'] != 'ok' ) {
+        return $rc;
+    }
 
-	$dh = $rc['dh'];
+    $dh = $rc['dh'];
 
-	//
-	// Prepare and Execute Query
-	//
-	$result = mysqli_query($dh, $strsql);
-	if( $result == false ) {
-		error_log("SQLERR: " . mysqli_error($dh) . " -- '$strsql'");
-		return array('stat'=>'fail', 'err'=>array('pkg'=>'ciniki', 'code'=>'3447', 'msg'=>'Database Error', 'pmsg'=>mysqli_error($dh)));
-	}
+    //
+    // Prepare and Execute Query
+    //
+    $result = mysqli_query($dh, $strsql);
+    if( $result == false ) {
+        error_log("SQLERR: " . mysqli_error($dh) . " -- '$strsql'");
+        return array('stat'=>'fail', 'err'=>array('pkg'=>'ciniki', 'code'=>'3447', 'msg'=>'Database Error', 'pmsg'=>mysqli_error($dh)));
+    }
 
-	//
-	// FIXME: If hash, then return all rows together as a hash
-	//
-	$rsp = array('stat'=>'ok');
-	$rsp['num_rows'] = 0;
+    //
+    // FIXME: If hash, then return all rows together as a hash
+    //
+    $rsp = array('stat'=>'ok');
+    $rsp['num_rows'] = 0;
 
-	//
-	// Build array of rows
-	//
-	$rsp[$container_name] = 0;
-	while( $row = mysqli_fetch_row($result) ) {
+    //
+    // Build array of rows
+    //
+    $rsp[$container_name] = 0;
+    while( $row = mysqli_fetch_row($result) ) {
         $rsp[$container_name] += $row[0];
-		$rsp['num_rows']++;
-	}
+        $rsp['num_rows']++;
+    }
 
-	mysqli_free_result($result);
+    mysqli_free_result($result);
 
-	return $rsp;
+    return $rsp;
 }
 ?>

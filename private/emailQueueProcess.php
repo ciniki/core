02208cd3dda5d42a23de46f2206bc4f70407f249
Andrew Rivett <veggiefrog@gmail.com>
2017-01-24 15:30:24 -0500

    Added ability for emailQueueProcess to use mailgun

--- private/emailQueueProcess.php
+++ private/emailQueueProcess.php
@@ -40,101 +40,181 @@ function ciniki_core_emailQueueProcess(&$ciniki) {
             }
         }
         elseif( isset($email['to']) ) {
-            require_once($ciniki['config']['ciniki.core']['lib_dir'] . '/PHPMailer/class.phpmailer.php');
-            require_once($ciniki['config']['ciniki.core']['lib_dir'] . '/PHPMailer/class.smtp.php');
-
-            $mail = new PHPMailer;
-            $mail->IsSMTP();
-
-            $use_config = 'yes';
+            //
+            // Get the business mail settings
+            //
             if( isset($email['business_id']) ) {
                 ciniki_core_loadMethod($ciniki, 'ciniki', 'mail', 'private', 'getSettings');
                 $rc = ciniki_mail_getSettings($ciniki, $email['business_id']);
-                if( $rc['stat'] == 'ok' && isset($rc['settings'])
-                    && isset($rc['settings']['smtp-servers']) && $rc['settings']['smtp-servers'] != ''
-                    && isset($rc['settings']['smtp-username']) && $rc['settings']['smtp-username'] != ''
-                    && isset($rc['settings']['smtp-password']) && $rc['settings']['smtp-password'] != ''
-                    && isset($rc['settings']['smtp-secure']) && $rc['settings']['smtp-secure'] != ''
-                    && isset($rc['settings']['smtp-port']) && $rc['settings']['smtp-port'] != ''
+                if( $rc['stat'] == 'ok' && isset($rc['settings']) ) {
+                    $settings = $rc['settings'];
+                }
+            }
+            //
+            // If mailgun is enabled
+            //
+            if( isset($settings['mailgun-domain']) && $settings['mailgun-domain'] != ''
+                && isset($settings['mailgun-key']) && $settings['mailgun-key'] != ''
+                ) {
+                //
+                // Setup the message
+                //
+                $msg = array(
+                    'from' => $settings['smtp-from-name'] . ' <' . $settings['smtp-from-address'] . '>',
+                    'subject' => $email['subject'],
+                    'html' => $email['htmlmsg'],
+                    'text' => $email['textmsg'],
+                    );
+                if( isset($ciniki['config']['ciniki.mail']['force.mailto']) ) {
+                    if( isset($email['to_name']) && $email['to_name'] != '' ) {
+                        $msg['to'] = $email['to_name'] . ' <' . $ciniki['config']['ciniki.mail']['force.mailto'] . '>';
+                    } else {
+                        $msg['to'] = $ciniki['config']['ciniki.mail']['force.mailto'];
+                    }
+                    $msg['subject'] .= ' [' . $email['to'] . ']';
+                } else {
+                    if( isset($email['to_name']) && $email['to_name'] != '' ) {
+                        $msg['to'] = $email['to_name'] . ' <' . $email['to'] . '>';
+                    } else {
+                        $msg['to'] = $email['to'];
+                    }
+                }
+
+                //
+                // Add attachments
+                //
+                $file_index = 1;
+                if( isset($email['attachments']) ) {
+                    foreach($email['attachments'] as $attachment) {
+                        if( isset($attachment['string']) ) {
+                            $tmpname = tempnam(sys_get_temp_dir(), 'mailgun');
+                            file_put_contents($tmpname, $attachment['string']);
+                            $cfile = curl_file_create($tmpname);
+                            $cfile->setPostFilename($attachment['filename']);
+                            $msg['file_' . $file_index] = $cfile;
+                            $file_index++;
+                        }
+                    }
+                }
+
+                //
+                // Send to mailgun api
+                //
+                $ch = curl_init();
+                curl_setopt($ch, CURLOPT_HTTPAUTH, CURLAUTH_BASIC);
+                curl_setopt($ch, CURLOPT_USERPWD, 'api:' . $settings['mailgun-key']);
+                curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+                curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'POST');
+                curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+                curl_setopt($ch, CURLOPT_HEADER, false);
+                curl_setopt($ch, CURLOPT_ENCODING, 'UTF-8');
+                curl_setopt($ch, CURLOPT_URL, 'https://api.mailgun.net/v3/' . $settings['mailgun-domain'] . '/messages');
+                curl_setopt($ch, CURLOPT_POSTFIELDS, $msg);
+
+                $rsp = json_decode(curl_exec($ch));
+
+                $info = curl_getinfo($ch);
+                if( $info['http_code'] != 200 ) {
+                    error_log("MAIL-ERR: [" . $email['to'] . "] " . $rsp->message);
+                }
+                curl_close($ch);
+            } 
+            //
+            // Otherwise use SMTP mailer
+            //
+            else {
+                require_once($ciniki['config']['ciniki.core']['lib_dir'] . '/PHPMailer/class.phpmailer.php');
+                require_once($ciniki['config']['ciniki.core']['lib_dir'] . '/PHPMailer/class.smtp.php');
+
+                $mail = new PHPMailer;
+                $mail->IsSMTP();
+
+                $use_config = 'yes';
+                if( isset($email['business_id']) 
+                    && isset($settings['smtp-servers']) && $settings['smtp-servers'] != ''
+                    && isset($settings['smtp-username']) && $settings['smtp-username'] != ''
+                    && isset($settings['smtp-password']) && $settings['smtp-password'] != ''
+                    && isset($settings['smtp-secure']) && $settings['smtp-secure'] != ''
+                    && isset($settings['smtp-port']) && $settings['smtp-port'] != ''
                     ) {
-                    $mail->Host = $rc['settings']['smtp-servers'];
+                    $mail->Host = $settings['smtp-servers'];
                     $mail->SMTPAuth = true;
-                    $mail->Username = $rc['settings']['smtp-username'];
-                    $mail->Password = $rc['settings']['smtp-password'];
-                    $mail->SMTPSecure = $rc['settings']['smtp-secure'];
-                    $mail->Port = $rc['settings']['smtp-port'];
+                    $mail->Username = $settings['smtp-username'];
+                    $mail->Password = $settings['smtp-password'];
+                    $mail->SMTPSecure = $settings['smtp-secure'];
+                    $mail->Port = $settings['smtp-port'];
                     $use_config = 'no';
 
-                    if( isset($rc['settings']['smtp-secure']) && $rc['settings']['smtp-secure'] != ''
-                        && isset($rc['settings']['smtp-port']) && $rc['settings']['smtp-port'] != '' ) {
-                        $mail->From = $rc['settings']['smtp-from-address'];
-                        $mail->FromName = $rc['settings']['smtp-from-name'];
+                    if( isset($settings['smtp-secure']) && $settings['smtp-secure'] != ''
+                        && isset($settings['smtp-port']) && $settings['smtp-port'] != '' ) {
+                        $mail->From = $settings['smtp-from-address'];
+                        $mail->FromName = $settings['smtp-from-name'];
                     } else {
                         $mail->From = $ciniki['config']['ciniki.core']['system.email'];
                         $mail->FromName = $ciniki['config']['ciniki.core']['system.email.name'];
                     }
-                }
-            } 
-            
-            //
-            // If not enough information, or none provided, default back to system email
-            //
-            if( $use_config == 'yes' ) {
-                if( !isset($ciniki['config']['ciniki.core']['system.email']) ) {
-                    // If the system.email is not set, don't send any emails, dev system.
-                    return array('stat'=>'ok');
-                }
-                $mail->Host = $ciniki['config']['ciniki.core']['system.smtp.servers'];
-                $mail->SMTPAuth = true;
-                $mail->Username = $ciniki['config']['ciniki.core']['system.smtp.username'];
-                $mail->Password = $ciniki['config']['ciniki.core']['system.smtp.password'];
-                $mail->SMTPSecure = $ciniki['config']['ciniki.core']['system.smtp.secure'];
-                $mail->Port = $ciniki['config']['ciniki.core']['system.smtp.port'];
+                } 
+                
+                //
+                // If not enough information, or none provided, default back to system email
+                //
+                if( $use_config == 'yes' ) {
+                    if( !isset($ciniki['config']['ciniki.core']['system.email']) ) {
+                        // If the system.email is not set, don't send any emails, dev system.
+                        return array('stat'=>'ok');
+                    }
+                    $mail->Host = $ciniki['config']['ciniki.core']['system.smtp.servers'];
+                    $mail->SMTPAuth = true;
+                    $mail->Username = $ciniki['config']['ciniki.core']['system.smtp.username'];
+                    $mail->Password = $ciniki['config']['ciniki.core']['system.smtp.password'];
+                    $mail->SMTPSecure = $ciniki['config']['ciniki.core']['system.smtp.secure'];
+                    $mail->Port = $ciniki['config']['ciniki.core']['system.smtp.port'];
 
-                $mail->From = $ciniki['config']['ciniki.core']['system.email'];
-                $mail->FromName = $ciniki['config']['ciniki.core']['system.email.name'];
-            }
+                    $mail->From = $ciniki['config']['ciniki.core']['system.email'];
+                    $mail->FromName = $ciniki['config']['ciniki.core']['system.email.name'];
+                }
 
-            if( isset($email['to_name']) && $email['to_name'] != '' ) {
-                $mail->AddAddress($email['to'], $email['to_name']);
-            } else {
-                $mail->AddAddress($email['to']);
-            }
-            
-            // Add reply to if specified
-            if( isset($email['replyto_email']) && $email['replyto_email'] != '' ) {
-                if( isset($email['replyto_name']) && $email['replyto_name'] != '' ) {
-                    $mail->addReplyTo($email['replyto_email'], $email['replyto_name']);
+                if( isset($email['to_name']) && $email['to_name'] != '' ) {
+                    $mail->AddAddress($email['to'], $email['to_name']);
                 } else {
-                    $mail->addReplyTo($email['replyto_email']);
+                    $mail->AddAddress($email['to']);
                 }
-            }
-
-            $mail->Subject = $email['subject'];
-            if( isset($email['htmlmsg']) && $email['htmlmsg'] != '' ) {
-                $mail->IsHTML(true);
-                $mail->Body = $email['htmlmsg'];
-                $mail->AltBody = $email['textmsg'];
-            } else {
-                $mail->IsHTML(false);
-                $mail->Body = $email['textmsg'];
-            }
-
-            //
-            // Check for attachments
-            //
-            if( isset($email['attachments']) ) {
-                foreach($email['attachments'] as $attachment) {
-                    if( isset($attachment['string']) ) {
-                        $mail->addStringAttachment($attachment['string'], $attachment['filename']);
+                
+                // Add reply to if specified
+                if( isset($email['replyto_email']) && $email['replyto_email'] != '' ) {
+                    if( isset($email['replyto_name']) && $email['replyto_name'] != '' ) {
+                        $mail->addReplyTo($email['replyto_email'], $email['replyto_name']);
+                    } else {
+                        $mail->addReplyTo($email['replyto_email']);
                     }
                 }
-            }
 
-            if( !$mail->Send() ) {
-                error_log("MAIL-ERR: [" . $email['to'] . "] " . $mail->ErrorInfo . " (trying again)");
+                $mail->Subject = $email['subject'];
+                if( isset($email['htmlmsg']) && $email['htmlmsg'] != '' ) {
+                    $mail->IsHTML(true);
+                    $mail->Body = $email['htmlmsg'];
+                    $mail->AltBody = $email['textmsg'];
+                } else {
+                    $mail->IsHTML(false);
+                    $mail->Body = $email['textmsg'];
+                }
+
+                //
+                // Check for attachments
+                //
+                if( isset($email['attachments']) ) {
+                    foreach($email['attachments'] as $attachment) {
+                        if( isset($attachment['string']) ) {
+                            $mail->addStringAttachment($attachment['string'], $attachment['filename']);
+                        }
+                    }
+                }
+
                 if( !$mail->Send() ) {
-                    error_log("MAIL-ERR: [" . $email['to'] . "] " . $mail->ErrorInfo);
+                    error_log("MAIL-ERR: [" . $email['to'] . "] " . $mail->ErrorInfo . " (trying again)");
+                    if( !$mail->Send() ) {
+                        error_log("MAIL-ERR: [" . $email['to'] . "] " . $mail->ErrorInfo);
+                    }
                 }
             }
         }

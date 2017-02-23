2b83dc962eb02f83a39536d640d2b53fa900932c
Andrew Rivett <veggiefrog@gmail.com>
2017-02-23 10:19:00 -0500

    Added API to return list of tables and their sizes

---
+++ public/checkDbTableSizes.php
@@ -0,0 +1,35 @@
+<?php
+//
+// Description
+// -----------
+// This function will return the size of tables in the database.
+//
+// Arguments
+// ---------
+//
+// Returns
+// -------
+//
+function ciniki_core_checkDbTableSizes($ciniki) {
+    //
+    // Check access restrictions to checkAPIKey
+    //
+    ciniki_core_loadMethod($ciniki, 'ciniki', 'core', 'private', 'checkAccess');
+    $rc = ciniki_core_checkAccess($ciniki, 0, 'ciniki.core.checkDbTableSizes');
+    if( $rc['stat'] != 'ok' ) {
+        return $rc;
+    }
+    error_log(print_r($ciniki['config']['ciniki.core'], true));
+    $strsql = "SELECT table_name, "
+        . "ROUND(((data_length + index_length) / 1024 / 1024), 2) AS mb "
+        . "FROM information_schema.TABLES "
+        . "WHERE table_schema = '" . ciniki_core_dbQuote($ciniki, $ciniki['config']['ciniki.core']['database.ciniki.database']) . "' "
+        . "ORDER BY mb DESC "
+        . "";
+    ciniki_core_loadMethod($ciniki, 'ciniki', 'core', 'private', 'dbHashQueryArrayTree');
+    $rc = ciniki_core_dbHashQueryArrayTree($ciniki, $strsql, 'ciniki.core', array(
+        array('container'=>'tables', 'fname'=>'table_name', 'fields'=>array('table_name', 'mb')),
+        ));
+    return $rc;
+}
+?>

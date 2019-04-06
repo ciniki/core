be8375c6075d0000761540fbe0ab52c61244e546
Andrew Rivett <veggiefrog@gmail.com>
2019-04-06 08:52:37 -0400

    Changed default response format from XML to JSON

--- private/printResponse.php
+++ private/printResponse.php
@@ -24,13 +24,13 @@ function ciniki_core_printResponse($ciniki, $hash) {
 
     //
     // If nothing is specified, or there's an error with the $ciniki data structure,
-    // then default to response in XML rest format.
+    // then default to response in JSON rest format.
     //
     if( !is_array($ciniki) || !is_array($ciniki['response']) || !isset($ciniki['response']['format']) ) {
-        header("Content-Type: text/xml; charset=utf-8");
-        print "<?xml version=\"1.0\" encoding=\"utf-8\" ?>\n";
-        ciniki_core_loadMethod($ciniki, 'ciniki', 'core', 'private', 'printHashToXML');
-        ciniki_core_printHashToXML('rsp', '', $hash);   
+        header("Content-Type: text/plain; charset=utf-8");
+        header("Cache-Control: no-cache, must-revalidate");
+        ciniki_core_loadMethod($ciniki, 'ciniki', 'core', 'private', 'printHashToJSON');
+        ciniki_core_printHashToJSON($hash);
     } 
 
     elseif( $ciniki['response']['format'] == 'php' ) {
@@ -39,21 +39,21 @@ function ciniki_core_printResponse($ciniki, $hash) {
         ciniki_core_printHashToPHP($hash);
     } 
 
-    elseif( $ciniki['response']['format'] == 'json' ) {
-        header("Content-Type: text/plain; charset=utf-8");
-        header("Cache-Control: no-cache, must-revalidate");
-        ciniki_core_loadMethod($ciniki, 'ciniki', 'core', 'private', 'printHashToJSON');
-        ciniki_core_printHashToJSON($hash);
+    elseif( $ciniki['response']['format'] == 'xml' ) {
+        header("Content-Type: text/xml; charset=utf-8");
+        print "<?xml version=\"1.0\" encoding=\"utf-8\" ?>\n";
+        ciniki_core_loadMethod($ciniki, 'ciniki', 'core', 'private', 'printHashToXML');
+        ciniki_core_printHashToXML('rsp', '', $hash);   
     }
 
     //
     // Default to XML rest response format if nothing else is specified.
     //
     else {
-        header("Content-Type: text/xml; charset=utf-8");
-        print "<?xml version=\"1.0\" encoding=\"utf-8\" ?>\n";
-        ciniki_core_loadMethod($ciniki, 'ciniki', 'core', 'private', 'printHashToXML');
-        ciniki_core_printHashToXML('rsp', '', $hash);   
+        header("Content-Type: text/plain; charset=utf-8");
+        header("Cache-Control: no-cache, must-revalidate");
+        ciniki_core_loadMethod($ciniki, 'ciniki', 'core', 'private', 'printHashToJSON');
+        ciniki_core_printHashToJSON($hash);
     }
 
 }

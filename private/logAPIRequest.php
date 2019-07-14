1c84ebc0da7a36f1048829b8d5b2fc21f259b44e
Andrew Rivett <veggiefrog@gmail.com>
2019-07-14 08:43:40 -0400

    Added ability for api logging to be turned off or switched from db to file

--- private/logAPIRequest.php
+++ private/logAPIRequest.php
@@ -5,42 +5,93 @@
 // This function will add a request to the api logs.
 //
 function ciniki_core_logAPIRequest($ciniki) {
-    //
-    // Log a API request 
-    //
-    $strsql = "INSERT INTO ciniki_core_api_logs (uuid, user_id, tnid, session_key, method, action, ip_address, "
-        . "log_date ) VALUES (uuid(), "
-        . "";
-    if( isset($ciniki['session']['user']['id']) ) {
-        $strsql .= "'" . ciniki_core_dbQuote($ciniki, $ciniki['session']['user']['id']) . "', ";
-    } else {
-        $strsql .= "0, ";
-    }
-    if( isset($ciniki['request']['args']['tnid']) ) {
-        $strsql .= "'" . ciniki_core_dbQuote($ciniki, $ciniki['request']['args']['tnid']) . "', ";
-    } else {
-        $strsql .= "0, ";
-    }
-    if( isset($ciniki['session']['change_log_id']) ) {
-        $strsql .= "'" . ciniki_core_dbQuote($ciniki, $ciniki['session']['change_log_id']) . "', ";
-    } else {
-        $strsql .= "'', ";
-    }
-    $strsql .= "'" . ciniki_core_dbQuote($ciniki, $ciniki['request']['method']) . "', "
-        . "";
-    if( isset($ciniki['request']['action']) ) {
-        $strsql .= "'" . ciniki_core_dbQuote($ciniki, $ciniki['request']['action']) . "', ";
-    } else {
-        $strsql .= "'', ";
-    }
-    if( isset($_SERVER['REMOTE_ADDR']) ) {
-        $strsql .= "'" . ciniki_core_dbQuote($ciniki, $_SERVER['REMOTE_ADDR']) . "', ";
-    } else {
-        $strsql .= "'localhost', ";
-    }
-    $strsql .= "UTC_TIMESTAMP())";
 
-    ciniki_core_loadMethod($ciniki, 'ciniki', 'core', 'private', 'dbInsert');
-    return ciniki_core_dbInsert($ciniki, $strsql, 'ciniki.core');
+    //
+    // Check if request should be logged to the file
+    //
+    if( isset($ciniki['config']['ciniki.core']['log_dir']) 
+        && isset($ciniki['config']['ciniki.core']['logging.api.file']) 
+        && $ciniki['config']['ciniki.core']['logging.api.file'] == 'yes'
+        ) {
+//        $logfile = $ciniki['config']['ciniki.core']['log_dir'] . '/api.
+        $dt = new DateTime('now', new DateTimezone('UTC'));
+        $msg = '[' . $dt->format('d/M/Y:H:i:s O') . ']';
+        if( isset($_SERVER['REMOTE_ADDR']) ) {
+            $msg .= " " . ciniki_core_dbQuote($ciniki, $_SERVER['REMOTE_ADDR']);
+        } else {
+            $msg .= " LOCALHOST";
+        }
+        if( isset($ciniki['request']['args']['tnid']) ) {
+            $msg .= " " . ciniki_core_dbQuote($ciniki, $ciniki['request']['args']['tnid']);
+        } else {
+            $msg .= " 0";
+        }
+        if( isset($ciniki['session']['user']['id']) ) {
+            $msg .= " " . ciniki_core_dbQuote($ciniki, $ciniki['session']['user']['id']);
+        } else {
+            $msg .= " 0";
+        }
+        if( isset($ciniki['session']['change_log_id']) ) {
+            $msg .= " " . ciniki_core_dbQuote($ciniki, $ciniki['session']['change_log_id']);
+        } else {
+            $msg .= " -";
+        }
+        $msg .= " " . $ciniki['request']['method'];
+        if( isset($ciniki['request']['action']) ) {
+            $msg .= " " . ciniki_core_dbQuote($ciniki, $ciniki['request']['action']);
+        } else {
+            $msg .= " -";
+        }
+
+        file_put_contents($ciniki['config']['ciniki.core']['log_dir'] . '/ciniki.core.api.' . $dt->format('Y-m') . '.log',
+            $msg . "\n", 
+            FILE_APPEND);
+    }
+
+    //
+    // Check if request should be logged to database
+    //
+    if( !isset($ciniki['config']['ciniki.core']['logging.api.db']) 
+        || $ciniki['config']['ciniki.core']['logging.api.db'] == 'yes'
+        ) {
+        $strsql = "INSERT INTO ciniki_core_api_logs (uuid, user_id, tnid, session_key, method, action, ip_address, "
+            . "log_date ) VALUES (uuid(), "
+            . "";
+        if( isset($ciniki['session']['user']['id']) ) {
+            $strsql .= "'" . ciniki_core_dbQuote($ciniki, $ciniki['session']['user']['id']) . "', ";
+        } else {
+            $strsql .= "0, ";
+        }
+        if( isset($ciniki['request']['args']['tnid']) ) {
+            $strsql .= "'" . ciniki_core_dbQuote($ciniki, $ciniki['request']['args']['tnid']) . "', ";
+        } else {
+            $strsql .= "0, ";
+        }
+        if( isset($ciniki['session']['change_log_id']) ) {
+            $strsql .= "'" . ciniki_core_dbQuote($ciniki, $ciniki['session']['change_log_id']) . "', ";
+        } else {
+            $strsql .= "'', ";
+        }
+        $strsql .= "'" . ciniki_core_dbQuote($ciniki, $ciniki['request']['method']) . "', "
+            . "";
+        if( isset($ciniki['request']['action']) ) {
+            $strsql .= "'" . ciniki_core_dbQuote($ciniki, $ciniki['request']['action']) . "', ";
+        } else {
+            $strsql .= "'', ";
+        }
+        if( isset($_SERVER['REMOTE_ADDR']) ) {
+            $strsql .= "'" . ciniki_core_dbQuote($ciniki, $_SERVER['REMOTE_ADDR']) . "', ";
+        } else {
+            $strsql .= "'localhost', ";
+        }
+        $strsql .= "UTC_TIMESTAMP())";
+        ciniki_core_loadMethod($ciniki, 'ciniki', 'core', 'private', 'dbInsert');
+        $rc = ciniki_core_dbInsert($ciniki, $strsql, 'ciniki.core');
+        if( $rc['stat'] != 'ok' ) {
+            return $rc;
+        }
+    }
+
+    return array('stat'=>'ok');
 }
 ?>

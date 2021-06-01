9d7093d97b70a4be3856e961ca898102526ed30e
Andrew Rivett <veggiefrog@gmail.com>
2021-06-01 14:59:18 -0400

    Fixed bug with date parsing when day of week included

--- private/parseArgs.php
+++ private/parseArgs.php
@@ -92,6 +92,7 @@ function ciniki_core_parseArgs(&$ciniki, $tnid, $raw_args, $arg_info) {
                 $args[$arg] = json_decode($raw_args[$arg], true);
             } elseif( isset($options['type']) && $options['type'] == 'date' && $raw_args[$arg] != '' ) {
                 $raw_args[$arg] = preg_replace('/Janurary/', 'January', $raw_args[$arg]);
+                $raw_args[$arg] = preg_replace('/(Sun|Mon|Tue|Wed|Thu|Fri|Sat) /', '', $raw_args[$arg]);
                 if( $raw_args[$arg] == 'now' || $raw_args[$arg] == 'today' ) {
                     $args[$arg] = strftime("%Y-%m-%d");
                 } elseif( $raw_args[$arg] == 'tomorrow' ) {
